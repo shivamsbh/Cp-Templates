a38d219e01f51f442075467b403381e870e28d50
shivamsbh <164722923+shivamsbh@users.noreply.github.com>
2024-07-05 02:42:21 +0530

    Update kruskal.cpp

--- kruskal.cpp
+++ kruskal.cpp
@@ -1,18 +1,19 @@
 /*
 use 0 based indexing please
 */
+/*template<class T>         bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }*/
+
 struct DSK {
 	vector<int> par, sizz;
 	int no_comp, max_csize = 1;
-	DSK(int n) : par(n), sizz(n, 1) {
+	DSK(int n): par(n), sizz(n, 1) {
 		iota(par.begin(), par.end(), 0);
 		no_comp = n;
 	}
 	int leader(int x) {
 		if (par[x] == x)
 			return x;
-		else
-			return (par[x] = leader(par[x]));
+		return (par[x] = leader(par[x]));
 	}
 	bool is_connected(int x, int y) {
 		return (leader(x) == leader(y));
@@ -21,20 +22,19 @@ struct DSK {
 		return sizz[leader(x)];
 	}
 	bool merge(int x, int y) {
-		int x_ = leader(x);
-		int y_ = leader(y);
-		if (x_ == y_)
+		int xpar = leader(x);
+		int ypar = leader(y);
+		if (xpar == ypar)
 			return false;
 		no_comp--;
-		if (size(x_) < size(y_))
-			swap(x_, y_);
-		sizz[x_] += sizz[y_];
-		max_csize = max(max_csize, sizz[x_]);
-		par[y_] = leader(x_);
+		if (size(xpar) < size(ypar))
+			swap(xpar, ypar);
+		sizz[xpar] += sizz[ypar];
+		par[ypar] = xpar;
+		ckmax(max_csize, size(xpar)); 
 		return true;
 	}
 };
-
 int Kruskal(int n, vector<vector<pair<int, int>>> &adj) {
 	priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> min_heap;
 	for (int i = 0; i < n; i++) {
