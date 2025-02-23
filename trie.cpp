3c1cac34f47bd7526502d69d0db6d7f1258de3fb
Shivam Saurabh <164722923+shivamsbh@users.noreply.github.com>
2025-02-24 01:39:39 +0530

    Create trie.cpp

---
+++ trie.cpp
@@ -0,0 +1,32 @@
+const int MAX_C = 26;
+const int MAX_S = 2e6;
+
+int Trie[MAX_S][MAX_C], cnt[MAX_S];
+int num = 0;
+
+void insert(string s) {
+    int u = 0;
+    for (char c : s) {
+        if (!Trie[u][c - 'A'])
+            Trie[u][c - 'A'] = ++num;
+        u = Trie[u][c - 'A'];
+    }
+    cnt[u]++;
+}
+
+bool search(string s) {
+    int u = 0;
+    for (char c : s) {
+        if (!Trie[u][c - 'A'])
+            return false;
+        u = Trie[u][c - 'A'];
+    }
+    return cnt[u] > 0;
+}
+
+// Reset between test cases
+void reset() {
+    memset(Trie, 0, sizeof(Trie));
+    memset(cnt, 0, sizeof(cnt));
+    num = 0;
+}
